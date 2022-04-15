598e063014faad8fe8b4eac7a146bebfabeb3e9b
vitor-mda <vitor-mda@hotmail.com>
2022-04-15 17:00:47 -0300

    ✨ feat: add Outputting Text exercise solution

---
+++ coding-exercises/1-outputting-text/exercise.rb
@@ -0,0 +1,10 @@
+# Use the puts function to output the text "I am a Ruby developer!"
+# Remember -- case sensitivity matters in Ruby.
+# All characters and casings must match the the ones in the string above.
+module Exercise1
+    def self.answer
+        "I am a Ruby developer!"
+    end
+end
+
+puts Exercise1.answer

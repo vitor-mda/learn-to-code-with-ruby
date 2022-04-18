aa948a0392c9fee1980d26b8ab218e677496ca21
vitor-mda <vitor-mda@hotmail.com>
2022-04-18 01:07:41 -0300

    ✨ feat: add Fizzbuzz challenge solution

--- coding-exercises/fizzbuzz-challenge/exercise.rb
+++ coding-exercises/fizzbuzz-challenge/exercise.rb
@@ -3,5 +3,16 @@ module FizzbuzzChallenge
         # If the number is divisible by 3, output Fizz
         # If the number is divisible by 5, output Buzz
         # If the number is divisible by BOTH 3 and 5, output Fizzbuzz
+        value = nil
+
+        value = "Fizz" if number % 3 == 0
+
+        if number % 5 == 0
+            if value.nil? then value = "Buzz"
+            else value.concat("buzz")
+            end
+        end
+
+        value
     end
 end

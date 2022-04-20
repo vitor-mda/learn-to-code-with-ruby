09b1a01b9ff772ead843a710d55709074d9f7fe6
vitor-mda <vitor-mda@hotmail.com>
2022-04-19 21:07:10 -0300

    ✨ feat: add .each_with_index Challenge solution

--- coding-exercises/each_with_index-method-challenge/exercise.rb
+++ coding-exercises/each_with_index-method-challenge/exercise.rb
@@ -2,5 +2,11 @@ module EachWithIndexChallenge
     def self.answer(array)
         # Write a loop that gives me a sum of
         # the products of each index and its value
+        return if array.nil? || !(array.is_a? Array) || array.empty?
+
+        sum = 0
+        array.each_with_index { |element, i| sum = sum + element * i}
+
+        sum
     end
 end

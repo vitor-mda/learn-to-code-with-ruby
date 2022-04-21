613c28f160e1538e76ba69d41a73b243917121b3
vitor-mda <vitor-mda@hotmail.com>
2022-04-21 01:33:14 -0300

    ✨ feat: add .each_with_index Method Challenge #2 solution

--- coding-exercises/each_with_index-method-challenge/exercise2.rb
+++ coding-exercises/each_with_index-method-challenge/exercise2.rb
@@ -1,7 +1,19 @@
 module EachWithIndexChallenge2
-    def self.answer(array)
+    def self.answer(numbers)
         # Prints the product of the element and its index pos.
         # if the index position is greater than the element
         # Create this within a method
+        return if numbers.nil? || !(numbers.is_a? Array) || numbers.empty?
+
+        printed_products = []
+        numbers.each_with_index do |number, index|
+            if index > number
+                product = number * index
+                puts "#{number} * #{index} = #{product}"
+                printed_products.push(product)
+            end
+        end
+
+        printed_products
     end
 end

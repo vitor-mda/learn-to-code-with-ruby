a592c2e43ceae8f8e3bc3ed9ac9c7daa2b2ea13f
vitor-mda <vitor-mda@hotmail.com>
2022-04-24 23:48:41 -0300

    ✨ feat: add Word Frequency challenge solution

--- coding-exercises/word-frequency-in-a-sentence-challenge/exercise.rb
+++ coding-exercises/word-frequency-in-a-sentence-challenge/exercise.rb
@@ -3,5 +3,12 @@ module WordFrequencyInASentenceChallenge
     # the words in the string and the values will
     # represent how many times that key occurs
     def self.answer(sentence)
+        frequency_by_word = Hash.new(0)
+
+        sentence.split.each do |word|
+            frequency_by_word[word] += 1
+        end
+
+        frequency_by_word
     end
 end

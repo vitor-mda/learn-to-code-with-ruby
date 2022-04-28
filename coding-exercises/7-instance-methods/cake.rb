e5c04be7711e9bd5a3754d032cc660f01b830287
vitor-mda <vitor-mda@hotmail.com>
2022-04-28 13:21:47 -0300

    ✨ feat: add Instance Methods exercise solution

--- coding-exercises/7-instance-methods/cake.rb
+++ coding-exercises/7-instance-methods/cake.rb
@@ -4,11 +4,14 @@
 #    - The sell method should return the string "Sold the cake"
 class Cake
     def bake
+        "Baking the cake"
     end
 
     def slice
+        "Slicing the cake"
     end
 
     def sell
+        "Sold the cake"
     end
 end

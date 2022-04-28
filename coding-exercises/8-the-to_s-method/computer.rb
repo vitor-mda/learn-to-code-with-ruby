41b42dd5dbdcccf6e756d4ee5ccea227145fad5f
vitor-mda <vitor-mda@hotmail.com>
2022-04-28 13:59:27 -0300

    🎨 style: fix indenting on `Computer` class

--- coding-exercises/8-the-to_s-method/computer.rb
+++ coding-exercises/8-the-to_s-method/computer.rb
@@ -5,14 +5,13 @@
 # The string should incorporate the 3 instance variables.
 
 class Computer
-    def initialize
-      @cpu = 'Intel i7'
-      @memory = 64
-      @storage = '2 TB'
-    end
-
-    def to_s
-        "A powerful #{@cpu} computer with #{@memory}GB memory and #{@storage} of storage"
-    end
+  def initialize
+    @cpu = 'Intel i7'
+    @memory = 64
+    @storage = '2 TB'
   end
-  
+
+  def to_s
+    "A powerful #{@cpu} computer with #{@memory}GB memory and #{@storage} of storage"
+  end
+end
